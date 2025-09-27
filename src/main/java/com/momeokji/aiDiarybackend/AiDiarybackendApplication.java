59cacf879e2a325f4f353bdb8620a7f1d155eae7
Elandland <68230815+Elandland@users.noreply.github.com>
2025-09-27 17:17:24 +0900

    Init: first commit

---
+++ src/main/java/com/momeokji/aiDiarybackend/AiDiarybackendApplication.java
@@ -0,0 +1,13 @@
+package com.momeokji.aiDiarybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDiarybackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiDiarybackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/momeokji/aiDiarybackend/AiDiarybackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.momeokji.aiDiarybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDiarybackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
