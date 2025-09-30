ec14a3180a27cb9a450d6a13d647a8c98e2f9fd9
Elandland <68230815+Elandland@users.noreply.github.com>
2025-10-01 02:14:37 +0900

    feat. 일기 생성 dto

---
+++ src/main/java/com/momeokji/aiDiarybackend/dto/response/DiaryGenerateResponseDto.java
@@ -0,0 +1,12 @@
+package com.momeokji.aiDiarybackend.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class DiaryGenerateResponseDto {
+
+	private String content;
+
+}
