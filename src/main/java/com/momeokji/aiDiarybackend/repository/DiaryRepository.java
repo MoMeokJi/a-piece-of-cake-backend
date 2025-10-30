51e423ef0fc178c582b570c6a1895021c611a14a
Elandland <68230815+Elandland@users.noreply.github.com>
2025-10-30 10:45:52 +0900

    edit. diary pk형식 string->long

--- src/main/java/com/momeokji/aiDiarybackend/repository/DiaryRepository.java
+++ src/main/java/com/momeokji/aiDiarybackend/repository/DiaryRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import com.momeokji.aiDiarybackend.entity.Diary;
 
 @Repository
-public interface DiaryRepository extends JpaRepository<Diary,String> {
+public interface DiaryRepository extends JpaRepository<Diary,Long> {
 
 
 }
