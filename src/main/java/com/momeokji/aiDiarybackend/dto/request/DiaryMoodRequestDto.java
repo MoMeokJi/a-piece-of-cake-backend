58d644b442aeee3b0201335f98ba75bb3e6ee7f2
jd30112 <jd30112@gmail.com>
2025-11-02 18:25:36 +0900

    feat: 음악추천 config, dto 생성

---
+++ src/main/java/com/momeokji/aiDiarybackend/dto/request/DiaryMoodRequestDto.java
@@ -0,0 +1,12 @@
+package com.momeokji.aiDiarybackend.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@NoArgsConstructor
+public class DiaryMoodRequestDto {
+    String content;
+
+}
