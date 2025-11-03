fe7561e79cd1b0c6040fa07198cce4008f3613ef
Elandland <68230815+Elandland@users.noreply.github.com>
2025-11-03 15:21:15 +0900

    feat. diary 월별,리스트,개별 조회 구현

--- src/main/java/com/momeokji/aiDiarybackend/controller/DiaryController.java
+++ src/main/java/com/momeokji/aiDiarybackend/controller/DiaryController.java
@@ -1,11 +1,15 @@
 package com.momeokji.aiDiarybackend.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.core.Authentication;
 
@@ -13,7 +17,9 @@ import com.momeokji.aiDiarybackend.dto.request.DiaryConfirmRequestDto;
 import com.momeokji.aiDiarybackend.dto.request.DiaryGenerateRequestDto;
 import com.momeokji.aiDiarybackend.dto.response.DailyQuestionsResponseDto;
 import com.momeokji.aiDiarybackend.dto.response.DiaryConfirmResponseDto;
+import com.momeokji.aiDiarybackend.dto.response.DiaryDetailResponseDto;
 import com.momeokji.aiDiarybackend.dto.response.DiaryGenerateResponseDto;
+import com.momeokji.aiDiarybackend.dto.response.DiaryListResponseDto;
 import com.momeokji.aiDiarybackend.service.DiaryService;
 import com.momeokji.aiDiarybackend.service.QuestionService;
 
@@ -44,6 +50,34 @@ public class DiaryController {
 
 		DiaryConfirmResponseDto response = diaryService.confirm(auth, req);
 		return ResponseEntity.ok(response);
+	}
+
+	@GetMapping
+	public List<DiaryListResponseDto> list(
+		Authentication auth,
+		@RequestParam(defaultValue = "recent") String sort,
+		@RequestParam(defaultValue = "0") Integer page,
+		@RequestParam(defaultValue = "10") Integer size
+	) {
+		return diaryService.getList(auth, sort, page, size);
+	}
+
+	@GetMapping("/calendar")
+	public ResponseEntity<List<DiaryListResponseDto>> getCalendar(
+		Authentication auth,
+		@RequestParam(required = false) Integer year,
+		@RequestParam(required = false) Integer month) {
+
+		List<DiaryListResponseDto> result = diaryService.getCalendar(auth, year, month);
+		return ResponseEntity.ok(result);
+	}
+
+
+	@GetMapping("/{diaryId}")
+	public ResponseEntity<DiaryDetailResponseDto> getDetail(
+		Authentication auth,
+		@PathVariable Long diaryId) {
+		return ResponseEntity.ok(diaryService.getDetail(auth, diaryId));
 	}
 
 }
