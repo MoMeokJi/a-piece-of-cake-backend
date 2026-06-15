2c006271d7a6bb6923bd7eb0d8628589c553c34b
Elandland <68230815+Elandland@users.noreply.github.com>
2026-06-15 14:40:32 +0900

    feat. Admin Entity추가

---
+++ src/main/java/com/momeokji/aiDiarybackend/entity/Admin.java
@@ -0,0 +1,68 @@
+package com.momeokji.aiDiarybackend.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@DynamicInsert
+@Table(name = "admin")
+public class Admin {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "admin_id", nullable = false, unique = true)
+	private String adminId;
+
+	@Column(name = "admin_pwd", nullable = false)
+	private String adminPwd;
+
+	@Column(name = "created_at", nullable = false)
+	private LocalDateTime createdAt;
+
+	@Column(name = "deleted_at")
+	private LocalDateTime deletedAt;
+
+	@Column(name = "is_valid", nullable = false, columnDefinition = "TINYINT DEFAULT 0")
+	@ColumnDefault("0")
+	private Boolean isValid;
+
+	@Column(name = "is_super", nullable = false, columnDefinition = "TINYINT DEFAULT 0")
+	@ColumnDefault("0")
+	private Boolean isSuper;
+
+	@Builder
+	public Admin(String adminId, String adminPwd, LocalDateTime createdAt, Boolean isValid, Boolean isSuper) {
+		this.adminId = adminId;
+		this.adminPwd = adminPwd;
+		this.createdAt = createdAt;
+		this.isValid = isValid;
+		this.isSuper = isSuper;
+	}
+
+	public void approve() {
+		this.isValid = true;
+	}
+
+	public void delete(LocalDateTime deletedAt) {
+		this.isValid = false;
+		this.deletedAt = deletedAt;
+	}
+}
