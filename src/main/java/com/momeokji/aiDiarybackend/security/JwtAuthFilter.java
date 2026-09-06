9d6f5b0b36a260525761843c87a87ff05aedb401
Elandland <68230815+Elandland@users.noreply.github.com>
2026-09-06 16:24:22 +0900

    edit. 관리자 설정에 music api추가

--- src/main/java/com/momeokji/aiDiarybackend/security/JwtAuthFilter.java
+++ src/main/java/com/momeokji/aiDiarybackend/security/JwtAuthFilter.java
@@ -248,16 +248,17 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 		return "ADMIN".equals(role) || "SUPER_ADMIN".equals(role);
 	}
 
-	/* 관리자 요청 컨트롤러 생기면 추가해야함*/
+
 	private boolean isAdminRequest(HttpServletRequest req) {
-		return isPath(req, "/admins")
-			|| isPath(req, "/dashboards");
+		return isAdminOnlyRequest(req);
 	}
 
+	/* 관리자 요청 컨트롤러 생기면 추가해야함*/
 	private boolean isAdminOnlyRequest(HttpServletRequest req) {
 		return isPath(req, "/admins")
 			|| isPath(req, "/questions")
-			|| isPath(req, "/dashboards");
+			|| isPath(req, "/dashboards")
+			|| isPath(req, "/musics");
 	}
 
 	private boolean isPath(HttpServletRequest req, String basePath) {
